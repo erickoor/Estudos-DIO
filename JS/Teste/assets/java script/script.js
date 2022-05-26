8e9d7de362e0f462a309a2c58f64dd07d03f309b
Erick Orzari <erick_orzari@hotmail.com>
2022-05-25 21:45:15 -0300

    ajustes

--- JS/Teste/assets/java script/script.js
+++ JS/Teste/assets/java script/script.js
@@ -1,12 +1,13 @@
-var currentNumberWrapper = document.getElementById('currentNumber')
-var currentNumber
+var currentNumber = 0;
+var currentNumberWrapper = document.getElementById("currentNumber");
+
 
 function increment() {
     currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber
+    currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber
+    currentNumberWrapper.innerHTML = currentNumber;
 }
